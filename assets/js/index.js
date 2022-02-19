8589d759cbf53d6ce4af544fe93b40e00c9b9da8
Samuel Jara <spjara.aravena@gmail.com>
2022-02-19 12:31:55 -0300

    Primer Commit

---
+++ assets/js/index.js
@@ -0,0 +1,13 @@
+import Leon from "./leon.js";
+import Lobo from "./lobo.js";
+import Oso from "./oso.js";
+import Serpiente from "./serpiente.js";
+import Aguila from "./aguila.js";
+
+$(() => {
+
+    $("#btnRegistrar").click(evento => {
+        evento.preventDefault();
+
+    })
+});
